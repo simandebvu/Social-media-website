6aee515cfd2c0fe20b6a39fc231baa3e047b6ffa
ricardo <ricardotvera.54@gmail.com>
2020-08-03 07:51:18 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,18 +20,7 @@ class User < ApplicationRecord
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array += inverted_friendships.map { |friendship| friendship.user if friendship.confirmed }
     friends_array.compact
-  end
-
-  # Users who have yet to confirme friend requests
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
-
-  # Users who have requested to be friends
-  def friend_requests
-    inverted_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
 
   def friend?(user)
